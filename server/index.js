8a47662de7d05879eadcce5504ec098259f91c1a
Chris Rauch <chris@chrisrau.ch>
2020-04-16 20:53:16 -0700

    add route

--- server/index.js
+++ server/index.js
@@ -24,4 +24,8 @@ app.get(`/api/reviews`, (req, res) => {
   })
 });
 
+app.get('http://3.101.42.194:3003/loaderio-c01ff4b7ab7aea1255175cda36ef807c/', (req, res) => {
+  res.status(200).json('hi');
+})
+
 app.listen(port, () => console.log(`listening on port ${port}!`));
