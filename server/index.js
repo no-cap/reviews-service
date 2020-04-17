7d79dfb9697ed5cfb67969609faf881a8c2d7a75
Chris Rauch <chris@chrisrau.ch>
2020-04-16 21:28:52 -0700

    setup loader

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.get(`/api/reviews`, (req, res) => {
 });
 
 app.get('loaderio-c01ff4b7ab7aea1255175cda36ef807c', (req, res) => {
-  res.status(200).json('hi');
+  res.status(200).json('loaderio-c01ff4b7ab7aea1255175cda36ef807c');
 })
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
